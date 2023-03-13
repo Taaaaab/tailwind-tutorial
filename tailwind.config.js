e4d44bde7c169f8b68e12a5d72dca4286f31f9e4
Taaaaab <owen.whitsett@gmail.com>
2023-03-13 07:56:05 -0400

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.html",
-    "./src/**/*.js",
-    "./public/**/*.html",
-    "./public/**/*.js",
-  ],
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+    },
   },
   plugins: [],
 };
